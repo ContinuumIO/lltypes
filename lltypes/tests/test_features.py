c7005cb3f6a1750d50e0ba47f9c5ac655db094ef
Stephen Diehl <stephen.m.diehl@gmail.com>
2013-06-26 10:13:26 -0500

    Demarcated tests

--- lltypes/tests/test_features.py
+++ lltypes/tests/test_features.py
@@ -1,5 +1,6 @@
 import sys
 import unittest
+import numpy as np
 
 is_py3k = bool(sys.version_info[0] == 3)
 
@@ -10,26 +11,36 @@ tests = []
 #------------------------------------------------------------------------
 
 class TestToplevel(unittest.TestCase):
-    def test_toplevle(self):
-        myarr = Sequence(Int8('a'), 128)
 
+    def test_struct_dtype(self):
         mystruct = Struct(
             'mystruct',
             Bool('a'),
             Int8('b'),
             Float32('c'),
         )
-
-        cstruct = mystruct.to_ctypes()
-        llstruct = mystruct.to_llvm()
-        dtstruct = mystruct.to_dtype()
-
-        inst = cstruct(
-            True,
-            3,
-            3.14
+        self.assertEqual(
+            mystruct.to_dtype(),
+            np.dtype([('a', '?'), ('b', 'i1'), ('c', '<f4')])
         )
 
+    def test_struct_llvm(self):
+        mystruct = Struct(
+            'mystruct',
+            Bool('a'),
+            Int8('b'),
+            Float32('c'),
+        )
+        llstruct = mystruct.to_llvm()
+        self.assertEqual(
+            str(llstruct),
+            """%mystruct = type { i1, i8, float }"""
+        )
+
+    def test_sequence(self):
+        myarr = Sequence(Int8('a'), 128)
+
+    def test_toplevle(self):
         c = Array_C('foo', UNInt8, 3)
         f = Array_F('foo', UNInt8, 3)
         s = Array_S('foo', UNInt8, 3)
